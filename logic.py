fec40222fdb00d18ab23151f83ec543995dac448
D-Guy Laptop <ddcool535@gmail.com>
2023-09-14 17:13:34 -0400

    Added equip time to weapon class

--- logic.py
+++ logic.py
@@ -5,7 +5,7 @@ import math
 
 class Weapon:
     """The class for weapon and weapon stats"""
-    def __init__(self, name, penetration, damage, mag_size, fire_rate, weight, length, bullet_accuracy,
+    def __init__(self, name, penetration, damage, mag_size, fire_rate, weight, length, equip_time, bullet_accuracy,
                  hip_fire_accuracy, aiming_accuracy, running_accuracy, **attachments):
         self.name = name
         self.penetration = penetration
@@ -14,6 +14,7 @@ class Weapon:
         self.fire_rate = fire_rate
         self.weight = weight
         self.length = length
+        self.equip_time = equip_time
         # Base accuracy
         self.bullet_accuracy = bullet_accuracy
         # Added to bullet acc with rounding
@@ -33,32 +34,32 @@ class Armor:
         self.limb_protection = limb_protection
 
 
-# name, pen, dmg, mag, rof, weight, length, bul_acc, hip_acc, aim_acc, run_acc, **attachments
+# name, pen, dmg, mag, rof, weight, length, equip, bul_acc, hip_acc, aim_acc, run_acc, **attachments
 # for **attachments, pass through list of available attachments by type -- Figure out how to do stats per attachment :/
 # Attachment Order: Sight, Body, Barrel, Stock, Bottom, Side, Magazine, other*
 # MTF Firearms
-com_15 = Weapon('COM-15', 20, 25, 12, 300, 0.65, 18, 0.2, 1.7, 0.25, 2.4,
+com_15 = Weapon('COM-15', 20, 25, 12, 300, 0.65, 18, 0.5, 0.2, 1.7, 0.25, 2.4,
                 barrel=['Standard Barrel', 'Suppressor'],
                 side=['None', 'Flashlight'],
                 magazine=['JHP Magazine', 'Extended JHP Magazine'])
-com_18 = Weapon('COM-18', 55, 21.2, 15, 420, 0.72, 19, 0.2, 1.7, 0.23, 2.4,
+com_18 = Weapon('COM-18', 55, 21.2, 15, 420, 0.72, 19, 0.26, 0.2, 1.7, 0.23, 2.4,
                 sight=['Iron Sights', 'Dot Sight'],
                 barrel=['Standard Barrel', 'Extended Barrel', 'Suppressor'],
                 side=['None', 'Laser Sight', 'Flashlight'],
                 magazine=['AP Magazine', 'JHP Magazine', 'Extended AP Magazine', 'Extended JHP Magazine'])
-fsp_9 = Weapon('FSP-9', 35, 22.3, 30, 690, 2.45, 45, 0.17, 1.7, 0.4, 2.5,
+fsp_9 = Weapon('FSP-9', 35, 22.3, 30, 690, 2.45, 45, 0.38, 0.17, 1.7, 0.4, 2.5,
                sight=['Iron Sights', 'Dot Sight', 'Holographic Sight'],
                barrel=['Standard Barrel', 'Flash Hider', 'Suppressor'],
                stock=['Retracted Stock', 'Extended Stock'],
                bottom=['Retracted Foregrip', 'Foregrip'],
                side=['None', 'Ammo Counter', 'Laser Sight', 'Flashlight'])
-crossvec = Weapon('Crossvec', 25, 23.5, 40, 750, 2.65, 61, 0.17, 1.7, 0.2, 2.8,
+crossvec = Weapon('Crossvec', 25, 23.5, 40, 750, 2.65, 61, 1.27, 0.17, 1.7, 0.2, 2.8,
                   sight=['Iron Sights', 'Holographic Sight', 'Dot Sight', 'Night Vision Scope'],
                   barrel=['Standard Barrel', 'Suppressor', 'Extended Barrel', 'Flash Hider'],
                   stock=['Stock', 'Retracted Stock'],
                   bottom=['None', 'Foregrip', 'Laser Sight', 'Flashlight'],
                   magazine=['JHP Magazine', 'Low-Cap AP Mag'])
-mtf_e11_sr = Weapon('MTF-E11-SR', 70, 25.1, 40, 570, 3.15, 87, 0.05, 2, 0.08, 5.8,
+mtf_e11_sr = Weapon('MTF-E11-SR', 70, 25.1, 40, 570, 3.15, 87, 0.73, 0.05, 2, 0.08, 5.8,
                     sight=['Iron Sight', 'Holographic Sight', 'Dot Sight', 'Night-Vision Scope', 'Telescopic Sight'],
                     body=['Carbine Body', 'Rifle Body'],
                     barrel=['Standard Barrel', 'Suppressor', 'Flash Hider', 'Muzzle Booster', 'Muzzle Brake'],
@@ -66,29 +67,29 @@ mtf_e11_sr = Weapon('MTF-E11-SR', 70, 25.1, 40, 570, 3.15, 87, 0.05, 2, 0.08, 5.
                     bottom=['None', 'Foregrip', 'Laser Sight'],
                     side=['None', 'Flashlight', 'Ammo Counter'],
                     magazine=['FMJ Magazine', 'FMJ Drum Magazine', 'Low-Cap JHP Magazine', 'Low-Cap AP Magazine'])
-fr_mg_0 = Weapon('FR-MG-0', 80, 22.9, 100, 750, 7.2, 102, 0.05, 3.5, 0.08, 10.9,
+fr_mg_0 = Weapon('FR-MG-0', 80, 22.9, 100, 750, 7.2, 102, 0.8, 0.05, 3.5, 0.08, 10.9,
                  sight=['Iron Sights', 'Dot Sight', 'Holographic Sight', 'Night Vision Scope', 'Telescopic Sight'],
                  barrel=['Standard Barrel', 'Muzzle Brake', 'Flash Hider', 'Short Barrel', 'Suppressor'],
                  stock=['Standard Stock', 'Heavy Stock'],
                  bottom=['None', 'Foregrip', 'Laser Sight', 'Flashlight'],
                  magazine=['FMJ Drum', 'AP Drum'])
 # Chaos Insurgency Firearms
-ak = Weapon('AK', 85, 26.2, 30, 498, 3.9, 96, 0.07, 2.5, 0.08, 7.4,
+ak = Weapon('AK', 85, 26.2, 30, 498, 3.9, 96, 0.54, 0.07, 2.5, 0.08, 7.4,
             sight=['Iron Sight', 'Holographic Sight', 'Ammo Counter Sight', 'Telescopic Sight'],
             barrel=['Standard Barrel', 'Extended Barrel', 'Suppressor', 'Muzzle Brake', 'Muzzle Booster'],
             stock=['Standard Stock', 'Heavy Stock', 'No Stock'],
             bottom=['None', 'Foregrip', 'Flashlight', 'Laser'],
             magazine=['Banana AP Mag', 'Banana JHP Mag', 'Drum AP Mag', 'Drum JHP Mag'])
-logicer = Weapon('Logicer', 90, 26.8, 100, 660, 11.3, 115, 0.035, 3.5, 0.15, 16.6,
+logicer = Weapon('Logicer', 90, 26.8, 100, 660, 11.3, 115, 0.93, 0.035, 3.5, 0.15, 16.6,
                  sight=['Iron Sights', 'Dot Sight', 'Ammo Counter Sight', 'Night-Vision Scope'],
                  barrel=['Standard Barrel', 'Flash Hider', 'Muzzle Brake', 'Light Barrel'],
                  bottom=['None', 'Foregrip', 'Flashlight', 'Laser Sight'])
-shotgun = Weapon('Shotgun', 70, 66.64, 14, 100, 3.95, 74, 0, 0.6, 0.1, 4.,
+shotgun = Weapon('Shotgun', 70, 66.64, 14, 100, 3.95, 74, 0.58, 0, 0.6, 0.1, 4.,
                  sight=['Iron Sights', 'Holographic Sight'],
                  barrel=['Standard Barrel', 'Choke', 'Extended Barrel'],
                  side=['None', 'Ammo Counter', 'Laser Sight', 'Flashlight'],
                  trigger=['Single-Shot System', 'Double-Shot System'])
-revolver = Weapon('.44 Revolver', 65, 57.77, 6, 240, 2.15, 34, 0.15, 2, 0.75, 2.7,
+revolver = Weapon('.44 Revolver', 65, 57.77, 6, 240, 2.15, 34, 0.45, 0.15, 2, 0.75, 2.7,
                   sight=['Iron Sights', 'Dot Sight', 'Telescopic Sight'],
                   barrel=['Medium Barrel', 'Long Barrel', 'Short Barrel'],
                   stock=['Standard Stock', 'Heavy Stock'],
